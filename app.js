c5bf7ff86bdb7e3f95bc3154a215dc9c95c27c0f
Thomas Hunter <tlhunter@gmail.com>
2012-02-28 14:23:10 -0500

    Setting app to use express

--- app.js
+++ app.js
@@ -1,21 +1,11 @@
-var app = require('http').createServer(handler),
-    io = require('socket.io').listen(app),
-    fs = require('fs');
+var app = require('express').createServer(),
+    io = require('socket.io').listen(app);
 
 app.listen(81);
 
-function handler (req, res) {
-  fs.readFile(__dirname + '/index.html',
-  function (err, data) {
-    if (err) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-
-    res.writeHead(200);
-    res.end(data);
-  });
-}
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
   socket.emit('message', { username: 'System', message: 'Initialized' });
