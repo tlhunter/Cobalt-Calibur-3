11febea86a2d5aa2b36af6f644c478def2ac6fa6
Thomas Hunter <tlhunter@gmail.com>
2012-03-06 20:28:07 -0500

    Adding some tilesets and graphics

--- map-builder.php
+++ map-builder.php
@@ -2,7 +2,11 @@
 $map = array();
 for($y = 0; $y < 200; $y++) {
     for($x = 0; $x < 200; $x++) {
-        $map[$x][$y] = array('g' => 1, 'b' => null);
+        $b = null;
+        if (rand(1, 100) <= 2) {
+            $b = 7;
+        }
+        $map[$x][$y] = array('g' => 1, 'b' => $b);
     }
 }
 echo json_encode($map);
