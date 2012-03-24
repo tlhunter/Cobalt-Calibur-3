6c4beb11778ade486a93f64fedd914c2a6318989
Thomas Hunter <tlhunter@gmail.com>
2012-03-24 12:54:13 -0400

    New map generating system :D

--- map-builder.php
+++ map-builder.php
@@ -1,11 +1,26 @@
 <?php
-$map = array();
-for($y = 0; $y < 200; $y++) {
-    for($x = 0; $x < 200; $x++) {
-        $map[$x][$y] = array(
-            0,
-            null
-        );
+$filename = "map-source.gif";
+$size = getimagesize($filename);
+$width = $size[0];
+$height = $size[1];
+
+$translation = array(
+    0 => 2, // Sand
+    1 => 7, // Small Rock
+    2 => 6, // Large Rock
+    3 => 1, // Dirt
+    4 => 3, // Water
+    5 => 4, // Tree
+    6 => 0 // Grass
+);
+
+$im = imagecreatefromgif($filename);
+$data = array();
+
+for ($x = 0; $x < $width; $x++) {
+    for ($y = 0; $y < $height; $y++) {
+        $rgb = imagecolorat($im, $x, $y);
+        $data[$x][$y] = array($translation[$rgb], null);
     }
 }
-echo json_encode($map);
+echo json_encode($data);
