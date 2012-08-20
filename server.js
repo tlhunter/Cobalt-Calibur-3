0f0e0d35c0ea37ae6698f67d8e7a923930120b2b
Derek Smith <dsmith.programmer@gmail.com>
2012-08-19 19:45:04 -0700

    Changed node's server address back to 127.0.0.1

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ var colors      = require('colors');
 
 // Web Server Configuration
 var server_port = 80; // most OS's will require sudo to listen on 80
-var server_address = '192.168.0.191'; //'127.0.0.1';
+var server_address = '127.0.0.1';
 
 // MongoDB Configuration
 var mongo_host = '127.0.0.1';
